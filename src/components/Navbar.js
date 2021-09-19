5ef9a98e8270b2e90f8b6fd752b2d6bbd621fe51
AKSHAY SINGH <74558781+Akshay2002Singh@users.noreply.github.com>
2021-09-19 20:46:02 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+const Navbar = ({ onSetIsClicked }) => {
+    const clicked = () => {
+        onSetIsClicked(1);
+    }
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-info">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Lets Call</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <button onClick={clicked} className="btn btn-outline-dark " type="submit">GET USER DATA</button>
+        </li>
+      </ul>
+        
+      
+    </div>
+  </div>
+</nav>
+</>
+    )
+}
+export default Navbar;
+
